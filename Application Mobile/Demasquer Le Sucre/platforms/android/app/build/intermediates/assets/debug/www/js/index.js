c753a0101e0cee00f1fd87b309b6468d96744e1d
Noureddine ADJOU <ad.noureddine@gmail.com>
2019-02-07 10:32:36 +0100

    MAJ Application mobile authentification avec AJAX

--- Application Mobile/Demasquer Le Sucre/platforms/android/app/build/intermediates/assets/debug/www/js/index.js
+++ Application Mobile/Demasquer Le Sucre/platforms/android/app/build/intermediates/assets/debug/www/js/index.js
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-var app = {
+ var app = {
     // Application Constructor
     initialize: function() {
         this.bindEvents();
@@ -47,3 +47,43 @@ var app = {
         console.log('Received Event: ' + id);
     }
 };
+
+cordova.plugins.barcodeScanner.scan(
+      function (result) {
+          alert("We got a barcode\n" +
+                "Result: " + result.text + "\n" +
+                "Format: " + result.format + "\n" +
+                "Cancelled: " + result.cancelled);
+      },
+      function (error) {
+          alert("Scanning failed: " + error);
+      },
+      {
+          preferFrontCamera : true, // iOS and Android
+          showFlipCameraButton : true, // iOS and Android
+          showTorchButton : true, // iOS and Android
+          torchOn: true, // Android, launch with the torch switched on (if available)
+          saveHistory: true, // Android, save scan history (default false)
+          prompt : "Place a barcode inside the scan area", // Android
+          resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+          formats : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
+          orientation : "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
+          disableAnimations : true, // iOS
+          disableSuccessBeep: false // iOS and Android
+      }
+   );
+   
+   
+      cordova.plugins.barcodeScanner.encode(cordova.plugins.barcodeScanner.Encode.TEXT_TYPE, "http://www.nytimes.com", function(success) {
+            alert("encode success: " + success);
+          }, function(fail) {
+            alert("encoding failed: " + fail);
+          }
+        );
+		
+/**/		
+$(function() {
+  $('#texteJQ').html('Hello world. Ce texte est affiché par jQuery.');
+});
+
+
