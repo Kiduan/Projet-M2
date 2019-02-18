66eb761d963957eaed4b1b35afd3ee1851cf44c5
Nicolas <nicolas.lenoach29@gmail.com>
2019-02-18 15:15:55 +0100

    ajout fonction paypal, v_temps_client maj + ajout v_validation_paiment pour IPN

--- Projet/vues/v_temps_client.php
+++ Projet/vues/v_temps_client.php
@@ -11,9 +11,24 @@
 /* ------------------------------------------------------- */-->
 
 <div class="titrecentre">Ajout de temps</div>
-
     <div class="fcentre">
+
+    <form align="center" action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post" target="_top">
+        <input type="hidden" name="cmd" value="_s-xclick">
+        <input type="hidden" name="hosted_button_id" value="Z4U3X4DR6529C">
+        <input type="hidden" name="return" value="http://localhost/sucre/projet/vues/v_paiement_valide.php">
+        <input type="hidden" name="notify_url" value="http://localhost/sucre/projet/vues/v_validation_paiement.php">
+        <table align="center">
+        <tr><td><input type="hidden" name="on0" value="Achat temps">Achat de temps</td></tr><tr><td><select name="os0">
+            <option value="1 mois -">1 mois - €10,00 EUR</option>
+            <option value="3 mois -">3 mois - €25,00 EUR</option>
+            <option value="1 an -">1 an - €100,00 EUR</option>
+        </select> </td></tr>
+        </table>
+        <input type="hidden" name="currency_code" value="EUR">
+        <input type="image" src="https://www.sandbox.paypal.com/fr_FR/FR/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal, le réflexe sécurité pour payer en ligne">
+        <img alt="" border="0" src="https://www.sandbox.paypal.com/fr_FR/i/scr/pixel.gif" width="1" height="1">
+    </form>
     
-                     
-        <div class='clear'></div>
+    <div class='clear'></div>
 </div>
---
+++ Projet/vues/v_validation_paiement.php
@@ -0,0 +1,51 @@
+<?php 
+// read the post from PayPal system and add 'cmd'
+$req = 'cmd=_notify-validate';
+
+foreach ($_POST as $key => $value) {
+    $value = urlencode(stripslashes($value));
+    $req .= "&$key=$value";
+}
+
+// post back to PayPal system to validate
+$header .= "POST /cgi-bin/webscr HTTP/1.0\r\n";
+$header .= "Content-Type: application/x-www-form-urlencoded\r\n";
+$header .= "Content-Length: " . strlen($req) . "\r\n\r\n";
+$fp = fsockopen ('ssl://www.paypal.com', 443, $errno, $errstr, 30);
+
+// assign posted variables to local variables
+$payment_status = $_POST['payment_status'];
+$payment_amount = $_POST['mc_gross'];
+
+if ( $payment_status == "Completed") {
+    // On se connecte à la BDD
+    try
+    {
+        $host = 'mysql:host=localhost;dbname=bdd_sucre'; 
+        $utilisateur = 'root';
+        $mdp = '';
+        $connection = new PDO ( $host, $utilisateur, $mdp); 
+    } catch ( Exception $e )
+    {
+        echo "Connection à MySQL impossible : ", $e->getMessage();
+        die(); 
+    }
+
+    if($payment_amount == '12')
+        $temps = '1';
+    else if ($payment_amount == '30')
+        $temps = '3';
+    else if ($payment_amount == '120')
+        $temps = '12';
+
+    // Requête d'insertion
+    $requete = $connection->prepare( '
+        UPDATE client
+        SET temps = :temps
+        WHERE client_id = :client_id;' );
+    $requete->execute( array(
+        ':client_id' => $_POST['client_id'],
+        ':temps' => $temps
+    ));
+}
+?>
