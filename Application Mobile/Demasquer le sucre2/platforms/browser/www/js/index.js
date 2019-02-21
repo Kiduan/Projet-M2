c714390980e831222e3264a621a638e93d46a3f9
Noureddine ADJOU <ad.noureddine@gmail.com>
2019-02-21 14:27:30 +0100

    Application OK

--- Application Mobile/Demasquer le sucre2/platforms/browser/www/js/index.js
+++ Application Mobile/Demasquer le sucre2/platforms/browser/www/js/index.js
@@ -16,27 +16,27 @@
  * specific language governing permissions and limitations
  * under the License.
  */
- var app = {
+var app = {
     // Application Constructor
-    initialize: function() {
+    initialize: function () {
         this.bindEvents();
     },
     // Bind Event Listeners
     //
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
+    bindEvents: function () {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
+    onDeviceReady: function () {
         app.receivedEvent('deviceready');
     },
     // Update DOM on a Received Event
-    receivedEvent: function(id) {
+    receivedEvent: function (id) {
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
@@ -47,45 +47,107 @@
         console.log('Received Event: ' + id);
     }
 };
+/*Fonction de scan des codes barres */
+function scanBarcode() {
 
-function scanBarcode(){
+    cordova.plugins.barcodeScanner.scan(
+        function (result) {
 
-cordova.plugins.barcodeScanner.scan(
-  function (result) {
-   /*   alert("We got a barcode\n" +
-            "Result: " + result.text + "\n" +
-            "Format: " + result.format + "\n" +
-            "Cancelled: " + result.cancelled);*/
-			
+            /*Si le resultat égal à 13 alors le code barre est correct */
+            if(result.text.length==13)
+            {
+            /*Nous mettons le flagScan à True */
+            localStorage.setItem("flagScan", "True");
+             /*Nous récuperons  le code barre */
+            localStorage.setItem("code", result.text);
+            }
+            else 
+            /*Sinon si le code barre > 0 donc nous avons une erreur de scan */
+             if (result.text.length > 0)
+             {
+                 localStorage.setItem("flagScan", "False");
+                 alert("le code barre "+result.text+" ne correspend pas au normes des codes barres, veuillez scanner le code de nouveau");
+             }
+               /* le cas code barre égal à 0 c'est le cas où on fait un retrour arriére avec la fleche sur le téléphone  */
+             /* Dans tous les cans nous alons actualiser la page avec la mis à jours de notre Flag */
+            location.reload();
+        },
 
-			localStorage.setItem("flagScan", "True");
-			localStorage.setItem("code", result.text);
-			
+        function (error) {
+            alert("Scanning failed: " + error);
+        }
+    );
+}
+/*La fonction de deconnexion supprime les variables globales  */
+function deconnexion() {
+    localStorage.removeItem("nom");
+    localStorage.removeItem("code");
+    localStorage.removeItem("temps");
+    localStorage.removeItem("flagScan");
+    localStorage.removeItem("morceaux");
+    window.location.replace("index.html");
 
-			location.reload();
-			
-			
-  }, 
-  
-  function (error) {
-      alert("Scanning failed: " + error);
-  }
- );
 }
 
-	function deconnexion()
-	{
-		localStorage.removeItem("nom");
-		localStorage.removeItem("code");
-		localStorage.removeItem("temps");
-		localStorage.removeItem("flagScan");
-		localStorage.removeItem("sucre");
-		window.location.replace("index.html");
-
-	}
 
 
+/*Fonction de scan à nouveau des codes barres */
 
+function reScanBarcode() {
 
+    cordova.plugins.barcodeScanner.scan(
+        function (result) {
 
+ /*Si le resultat égal à 13 alors le code barre est correct */
+            if(result.text.length==13)
+            {
+            localStorage.setItem("code", result.text);
+            getSucre();
+            }
+            else
+           /*Sinon si le code barre > 0 donc nous avons une erreur de scan */
+           {
+              if (result.text.length > 0)
+                {
+                alert("le code barre "+result.text+" ne correspend pas au normes des codes barres, veuillez scanner le code de nouveau");
+                
+                }
+             /* le cas code barre égal à 0 c'est le cas où on fait un retrour arriére avec la fleche sur le téléphone  */
+                 location.reload();
+            }   
 
+        },
+
+        function (error) {
+            alert("Scanning failed: " + error);
+        }
+    );
+}
+
+/*Fonction qui récupère le sucre à partir de serveur */
+function getSucre() {
+    $.ajax({
+       /*Initialisation de la requete ajax */
+        type: "GET",
+        url: 'http://172.31.7.30/api/code_barre.php',
+        data: "code=" + localStorage.getItem("code"), 
+        success: function (data) {
+            /*On est sensé de récupérer un objet JSON */
+            try  {
+                /*Parse de l'objet  */
+                var obj = JSON.parse(data);
+                /*La variable globale Morceau reçoit les morceaux  */
+                localStorage.setItem("morceaux", Math.round(parseFloat(obj.morceaux)));;
+            /*Ancienne version on récupère l'url de l'image de openfoodfact  */
+                var image = obj.url;
+                /*redéraction */
+                window.location.replace("scan2.html");
+            } catch(e)
+            {
+                alert("Le produit n'existe pas dans la base!");
+            }
+        }
+
+    });
+
+}
